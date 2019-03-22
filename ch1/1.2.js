32e0b7b49ec91fa0ab9891a3726990dfdbbf0b2a
Dylan Wright <sealsurlaw@gmail.com>
2019-03-22 01:13:34 -0700

    Fixed 1.2

--- ch1/1.2.js
+++ ch1/1.2.js
@@ -7,11 +7,17 @@ let s4 = 'fizzbuzz';
 function isPermutation(str1, str2) {
 	if (str1.length != str2.length) return false;
 
+	let foundChar = new Array(128);
+	foundChar.fill(0);
+
+	for (let i = 0; i < str1.length; i++) {
+		foundChar[str1.charCodeAt(i)]++;
+		foundChar[str2.charCodeAt(i)]--;
+	}
 
 	let sum = 0;
-	for (let i = 0; i < str1.length; i++) {
-		sum += str1.charCodeAt(i);
-		sum -= str2.charCodeAt(i);
+	for (let i = 0; i < foundChar.length; i++) {
+		sum += foundChar[i];
 	}
 	if (sum == 0) return true;
 
